72ad4f5d8b0d7512b774054bdf0b2f6d9d3b1288
Dominique Tardif <dommtardif@gmail.com>
2018-10-30 15:58:59 -0400

    Modified startup of estation for linux using xterm to provide tty to emulators. Added workaround in settings for windows stacking issue. Added option to disable powersavings if they cause issues.

--- script.kodi.launches.emulationstation/addon.py
+++ script.kodi.launches.emulationstation/addon.py
@@ -7,7 +7,7 @@ import xbmcgui
 import platform
 import os.path
 import subprocess
-
+import json
 
 # Getting constants
 __addon__ = xbmcaddon.Addon('script.kodi.launches.emulationstation')
@@ -28,18 +28,31 @@ log("Starting " + __addonName__)
 # Var to hold  the path where the EmulationStation executable is
 executable = ""
 
+# Var to hold the path where Xterm for linux installations
+xterm = ""
+
+# Var to hold launch command
+launch = []
+
 # We are going to ask on which platform this is running so we can load the default executable location for it
 if platform.system() == "Windows":
     # If the platform is Windows
     executable = __addon__.getSetting('windowsExecutable').decode('utf-8')
     log("Loaded Windows executable location from Settings: " + executable)
-
+    launch = [executable]
 else:
     # Otherwise it is Linux because EmulationStation currently runs on Windows, Debians and RetroPie
     executable = __addon__.getSetting('linuxExecutable').decode('utf-8')
     log("Loaded Linux executable location from Settings: " + executable)
-
-
+    xterm = __addon__.getSetting('xtermExecutable').decode('utf-8')
+    log("Load xterm executable location from Settings: " + xterm)
+    # Verify that the xterm path is still valid
+    if not os.path.isfile(xterm):
+        # This message prints something like: Xterm executable was not found, go to Addon-Configure and change it
+        xbmcgui.Dialog().ok(__localizedMessages__(32000), __localizedMessages__(32001) ,__localizedMessages__(32002), __localizedMessages__(32003) + xterm)
+        # Log the error
+        log("xterm executable was not found on this specified location: " + xterm, xbmc.LOGERROR)
+    launch = [xterm, "-fullscreen" ,"-e", executable]
 # When you choose an executable from Kodi when changing Addon Settings, Kodi forces you to select an executable that exists, but we want to validate
 # if the executable exists anyways, just in case the default value doesn't work or the settings were manually changed
 if not os.path.isfile(executable):
@@ -51,5 +64,29 @@ if not os.path.isfile(executable):
     log("EmulationStation executable was not found on this specified location: " + executable, xbmc.LOGERROR)
 
 else:
+    # Var to hold current display sleep timer
+    displayoff = ""
+
+    # Var to hold current computer sleep timer
+    shutdowntime = ""
+
     log("Starting EmulationStation executable: " + executable)
-    subprocess.call([executable])
+    if __addon__.getSetting('powersaving') == "true":
+        log("Turning off powersavings")
+        displayoff = str(json.loads(xbmc.executeJSONRPC('{"jsonrpc":"2.0","method":"Settings.GetSettingValue", "params":{"setting":"powermanagement.displaysoff"},"id":1}'))["result"]["value"])
+        log("Current display off time: " + displayoff)
+        shutdowntime = str(json.loads(xbmc.executeJSONRPC('{"jsonrpc":"2.0","method":"Settings.GetSettingValue", "params":{"setting":"powermanagement.shutdowntime"},"id":1}'))["result"]["value"])
+        log("Current shutdown time: " + shutdowntime)
+        xbmc.executeJSONRPC('{"jsonrpc":"2.0","method":"Settings.SetSettingValue", "params":{"setting":"powermanagement.displaysoff","value":0},"id":1}')
+        xbmc.executeJSONRPC('{"jsonrpc":"2.0","method":"Settings.SetSettingValue", "params":{"setting":"powermanagement.shutdowntime","value":0},"id":1}')
+    if __addon__.getSetting('workaround') == "true":
+        log("Applying workaround for windows stacking order")
+        xbmc.executeJSONRPC('{"jsonrpc":"2.0","method": "Input.ExecuteAction", "params": {"action":"togglefullscreen"},"id":1}')
+    subprocess.call(launch)
+    if __addon__.getSetting('powersaving') == "true":
+        log("Returning powersaving settings to inital value")
+        xbmc.executeJSONRPC('{"jsonrpc":"2.0","method":"Settings.SetSettingValue", "params":{"setting":"powermanagement.displaysoff","value":'+displayoff+'},"id":1}')
+        xbmc.executeJSONRPC('{"jsonrpc":"2.0","method":"Settings.SetSettingValue", "params":{"setting":"powermanagement.shutdowntime","value":'+shutdowntime+'},"id":1}')
+    if __addon__.getSetting('workaround') == "true":
+	log("Removing workaround for windows stacking order")
+        xbmc.executeJSONRPC('{"jsonrpc":"2.0","method": "Input.ExecuteAction", "params": {"action":"togglefullscreen"},"id":1}')
